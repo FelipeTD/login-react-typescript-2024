fa53de755b2cbc64093720dcb478fee9192bc4bd
filipe.dias <fedispato@gmail.com>
2024-05-08 21:58:53 -0300

    feat: estrutura inicial

---
+++ login/src/contexts/Auth/AuthProvider.tsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { AuthContext } from "./AuthContext";
+import { User } from "../../types/User";
+
+export const AuthProvider = ({ children }: { children: JSX.Element }) => {
+
+    const [user, setUser] = useState<User | null>(null);
+
+    const signin = async () => {
+
+        setUser({
+            "id": 1, 
+            "name": "fedispato", 
+            "email": "fedispato@gmail.com"
+        });
+
+        return false;
+    }
+
+    const signout = async () => {
+
+    }
+
+    return(
+        <AuthContext.Provider value={{ user, signin, signout }}>
+            {children}
+        </AuthContext.Provider>
+    );
+
+}
---
+++ login/src/pages/Private/index.tsx
@@ -0,0 +1,7 @@
+export const Private = () => {
+    return (
+        <div>
+            <h2>Página Privada</h2>
+        </div>
+    )
+}
